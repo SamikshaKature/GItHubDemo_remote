b5cb2af6efd161868685101471a367f811bf06be
SamikshaKature <74428521+SamikshaKature@users.noreply.github.com>
2023-01-14 11:41:23 -0500

    Initial Commit

---
+++ GItHubDemo/GItHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GItHubDemoApp.swift
+//  GItHubDemo
+//
+//  Created by Samiksha Kature on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GItHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
